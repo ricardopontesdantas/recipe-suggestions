a59372fc01ec4f31c16fd276ce1f29f10d81eab8
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-04 07:25:25 -0300

    feat(create): create scripts sql

--- create.sql
+++ create.sql
@@ -13,17 +13,32 @@ create table recipes.ingredient (
 create table recipes.recipe_ingredient (
     id_recipe text,
     id_ingredient integer,
-    quantity integer
+    quantity text
 );
 
 create table recipes.stock (
-    id_ingredient integer
-    type integer
+    id_ingredient integer,
+    type integer,
     quantity integer
 );
 
-insert into recipes.recipe () values ();
+insert into recipes.recipe (id_recipe, description) values 
+('8a0ba637-792a-44ca-8957-63845d260524', 'omelete');
 
-insert into recipes.ingredient () values ();
+insert into recipes.ingredient (id_ingredient, description) values 
+(1, 'ovo'),
+(2, 'pimentão verde'),
+(3, 'cebola'),
+(4, 'sal');
 
-insert into recipes.stock () values ();
+insert into recipes.recipe_ingredient (id_recipe, id_ingredient, quantity) values 
+('8a0ba637-792a-44ca-8957-63845d260524', 1, '2'),
+('8a0ba637-792a-44ca-8957-63845d260524', 2, '1/2'),
+('8a0ba637-792a-44ca-8957-63845d260524', 3, '1/4'),
+('8a0ba637-792a-44ca-8957-63845d260524', 4, '1/2 colher de sopa');
+
+insert into recipes.stock (id_stock, id_ingredient, type, quantity) values 
+(1, 1, 1, 12),
+(2, 2, 1, 2),
+(3, 3, 1, 4),
+(4, 4, 2, 2),
