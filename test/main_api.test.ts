de5999580e0366874fa25ce663aa0d06c026e7c0
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-05 08:29:47 -0300

    test(main_api): create new recipe test

--- test/main_api.test.ts
+++ test/main_api.test.ts
@@ -5,3 +5,20 @@ test("shoud show hello world message", async function() {
     const output = response.data;
     expect(output.message).toBe("hello world");
 })
+
+test("should create a new recipe", async function() {
+    const input = {
+        idRecipe: "8a0ba637-792a-44ca-8957-63845d260524",
+        description: "omelete",
+        ingredients: [
+            { idIngredient: 1, quantity: "2" },
+            { idIngredient: 2, quantity: "1/2" },
+            { idIngredient: 3, quantity: "1/4" },
+            { idIngredient: 4, quantity: "1/2 colher de sopa" },
+        ]
+    }
+    const response = await axios.post("http://localhost:3000/recipe", input);
+    const output = response.data;
+    expect(output.description).toBe("omelete");
+    expect(output.ingredients.length).toBe(4);
+});
