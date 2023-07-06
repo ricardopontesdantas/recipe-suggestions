3ef0aa91a9cc8661581458d425b3ea8621092ee9
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-06 08:47:17 -0300

    feat(main_api): create validation to invalid body

--- src/main_api.ts
+++ src/main_api.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
 const app = express();
-app.get("/hello-world", function(request: Request, response: Response) {
-    return response.json({ message: "hello world" })
-});
+app.use(express.json());
+app.post("/recipes", function(request: Request, response: Response) {
+    if (!request.body.description) {
+        return response.json({
+            message: "Invalid description"
+        });
+    }
+    if (!request.body.ingredients.length) {
+        return response.json({
+            message: "Invalid ingredients"
+        });
+    }
+})
 app.listen(3000);
