48d1ddc004f2e38fa58f76bd9351cda9a9d12438
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-10 16:11:34 -0300

    test(create-recipe): create test for create recipe use case

---
+++ test/CreateRecipe.test.ts
@@ -0,0 +1,17 @@
+import CreateRecipe from "../src/CreateRecipe";
+
+test("should create a new recipe", async function() {
+    const input = {
+        idRecipe: "8a0ba637-792a-44ca-8957-63845d260524",
+        description: "omelete",
+        ingredients: [
+            { idIngredient: 1, quantity: "2" },
+            { idIngredient: 2, quantity: "1/2" },
+            { idIngredient: 3, quantity: "1/4" },
+            { idIngredient: 4, quantity: "1/2 colher de sopa" },
+        ]
+    }
+    const createRecipe = new CreateRecipe();
+    const output = await createRecipe.execute(input);
+    expect(output.description).toBe("omelete");
+});
