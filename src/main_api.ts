d760eb5c77fa6f2f307027f97b610648353c3159
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-11 11:00:29 -0300

    feat(main_api): call use case create recipe on api separating responsibilities

--- src/main_api.ts
+++ src/main_api.ts
@@ -1,20 +1,16 @@
 import express, { Request, Response } from "express";
+import CreateRecipe from "./CreateRecipe";
 const app = express();
 app.use(express.json());
-app.post("/recipes", function(request: Request, response: Response) {
-    if (!request.body.description) {
-        return response.json({
-            message: "Invalid description"
-        });
+app.post("/recipes", async function(request: Request, response: Response) {
+    const createRecipe = new CreateRecipe();
+    try {
+        const output = await createRecipe.execute(request.body);
+        return response.json(output);
+    } catch (e: any) {
+        return response.status(422).json({
+            message: e.message
+        })
     }
-    if (!request.body.ingredients.length) {
-        return response.json({
-            message: "Invalid ingredients"
-        });
-    }
-    const output = {
-        description: request.body.description
-    }
-    return response.json(output);
 });
 app.listen(3000);
