951e4f7d78621285781dcb7dfe5690d14c3a369b
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-15 11:40:57 -0300

    feat(create-recipe): get ingredients from database and add toa  recipe

--- src/CreateRecipe.ts
+++ src/CreateRecipe.ts
@@ -1,9 +1,25 @@
+import IngredientRepositoryDatabase from "./IngredientRepositoryDatabase";
+import RecipeRepositoryDatabase from "./RecipeRepositoryDatabase";
+
 export default class CreateRecipe {
     async execute(input: Input): Promise<Output> {
         if (!input.description) throw new Error("Invalid description");
         if (!input.ingredients.length) throw new Error("Invalid ingredients");
+        const recipe: Recipe = {
+            idRecipe: input.idRecipe,
+            description: input.description,
+            ingredients: []
+        }
+        const ingredientRepositoryDatabase = new IngredientRepositoryDatabase();
+        for (const ingredient of input.ingredients) {
+            const ingredientData = await ingredientRepositoryDatabase.get(ingredient.idIngredient);
+            recipe.ingredients.push(ingredientData);
+        }
+        const recipeRepositoryDatabase = new RecipeRepositoryDatabase();
+        await recipeRepositoryDatabase.save(recipe);
         const output = {
-            description: input.description
+            description: input.description,
+            amountIngredients: input.ingredients.length
         }
         return output;
     }
@@ -16,5 +32,12 @@ type Input = {
 }
 
 type Output = {
-    description: string
+    description: string,
+    amountIngredients: number
+}
+
+type Recipe = {
+    idRecipe: string,
+    description: string,
+    ingredients: any[]
 }
