31accd27e8296ff5bf635f4e63a2f6de2ce0b9d3
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-16 14:20:50 -0300

    feat(recipe-repository-database): insert data into table recipe_ingredients

--- src/RecipeRepositoryDatabase.ts
+++ src/RecipeRepositoryDatabase.ts
@@ -5,6 +5,9 @@ export default class RecipeRepositoryDatabase implements RecipeRepository {
     async save(recipe: any): Promise<void> {
         const connection = pgp()("postgres://docker:ignite@localhost:5432/local");
         await connection.query("insert into recipes.recipe (id_recipe, description) values ($1, $2)", [recipe.idRecipe, recipe.description]);
+        for (const ingredient of recipe.ingredients) {
+            await connection.query("insert into recipes.recipe_ingredient (id_recipe, id_ingredient, quantity) values ($1, $2, $3)", [recipe.idRecipe, ingredient.idIngredient, ingredient.quantity]);
+        }
         await connection.$pool.end();
     }
 }
