987cebe41598799c2beb921c049a8140a3ee094f
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-22 08:57:48 -0300

    feat(recipe): create a test for add an ingredient in a recupe

--- test/Recipe.test.ts
+++ test/Recipe.test.ts
@@ -6,3 +6,13 @@ test("should create a recipe", function() {
     const recipe = new Recipe(idRecipe, "omelete");
     expect(recipe.idRecipe).toBe(idRecipe);
 });
+
+test("should add an ingredient", function() {
+    const idRecipe = crypto.randomUUID();
+    const recipe = new Recipe(idRecipe, "omelete");
+    recipe.addIngredient({ idIngredient: 1, quantity: "2" });
+    recipe.addIngredient({ idIngredient: 2, quantity: "1/2" });
+    recipe.addIngredient({ idIngredient: 3, quantity: "1/4" });
+    recipe.addIngredient({ idIngredient: 4, quantity: "1/2 colher de sopa" });
+    expect(recipe.ingredients.length).toBe(4);
+});
