77dafd14c7d5a5e03cfc653142b7225198fdb2f1
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-07-22 08:58:15 -0300

    feat(recipe): create method add ingredient

--- src/Recipe.ts
+++ src/Recipe.ts
@@ -1,3 +1,9 @@
 export default class Recipe {
+    ingredients: any[] = [];
+
     constructor(readonly idRecipe: string, readonly description: string) {}
+
+    addIngredient(ingredient: any) {
+        this.ingredients.push(ingredient);
+    }
 }
